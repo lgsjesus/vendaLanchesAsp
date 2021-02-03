3d38e78a0047d2013bd76ab0bbf77fb5ac946933
lgsjesus <lguilherme.j@gmail.com>
2021-02-03 16:00:51 -0300

    Inserido Fonts Awesome

---
+++ vendaLanchesAsp/vendaLanchesAsp/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vendaLanchesAsp.Models;
+using vendaLanchesAsp.ViewModels;
+
+namespace vendaLanchesAsp.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+        public IViewComponentResult Invoke()
+        {
+            //var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            var itens = new List<CarrinhoCompraItem>() { new CarrinhoCompraItem() };
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+            var carrinhoView = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                TotalCarrinhoCompra = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhoView);
+        }
+    }
+}
--- vendaLanchesAsp/vendaLanchesAsp/Controllers/LanchesController.cs
+++ vendaLanchesAsp/vendaLanchesAsp/Controllers/LanchesController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using vendaLanchesAsp.Models;
 using vendaLanchesAsp.Repositories;
 using vendaLanchesAsp.ViewModels;
 
@@ -21,15 +22,33 @@ namespace vendaLanchesAsp.Controllers
         {
             return View();
         }
-        public IActionResult List()
+        public IActionResult List(string categoria)
         {
             ViewBag.Lanche = "Lanches";
             ViewData["Categorias"] = "Categoria";
+            IEnumerable<Lanche> _listaLanches = null;
+            string _categoria = "Todas as Categorias";
+            if (string.IsNullOrEmpty(categoria))
+            {
+                _listaLanches = _lanche.Lanches.OrderBy(v=>v.Nome);
 
+            }
+            else if (categoria.ToUpper().Equals("NATURAL"))
+            {
+                _listaLanches = _lanche.Lanches.Where(l => l.Categoria.CategoriaNome.Equals("Natural")).OrderBy(l => l.Nome);
+                _categoria = "Natural";
+            }
+            else if (categoria.ToUpper().Equals("NORMAL"))
+            {
+                _listaLanches =  _lanche.Lanches.Where(l => l.Categoria.CategoriaNome.Equals("Normal")).OrderBy(l => l.Nome);
+                _categoria = "Normal";
+            }
             //return View(_lanche.Lanches);
             LancheViewModel model = new LancheViewModel();
             model.Lanches = _lanche.Lanches;
             model.Categoria = "Categoria Atual";
+            model.Lanches = (IEnumerable<Models.Lanche>)_listaLanches;
+            model.Categoria = _categoria;
             return View(model);
         }
 
